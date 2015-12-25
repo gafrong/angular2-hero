f43165a6efe77ffcc959c8cf0941191787c29b53
Ji Lee <gafrong@gmail.com>
2015-12-25 16:38:00 +1100

    service updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,15 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-app',
-  template:`
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="#hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `,
+  templateUrl:'app/view/my-component.html',
   styles:[`
-    .heroes {
-      list-style-type: none; 
-      margin-left: 1em; 
-      padding: 0; 
-      width: 10em;
-    }
-    .heroes li { 
-      cursor: pointer; 
-      position: relative; 
-      left: 0; 
-      transition: all 0.2s ease; 
-    }
-    .heroes li:hover {
-      color: #369; 
-      background-color: #EEE; 
-      left: .2em;
-    }
+    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
     .heroes .badge {
       font-size: small;
       color: white;
@@ -44,31 +20,31 @@ import {HeroDetailComponent} from './hero-detail.component';
       left: -1px;
       top: -1px;
     }
-    .selected { 
-      background-color: #EEE; 
-      color: #369; 
-    }
+    .selected { background-color: #EEE; color: #369; }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-export class AppComponent {
-  public title = 'Tour of Heroes';
-  public heroes = HEROES;
+
+export class AppComponent implements OnInit {
+  public title = 'Names of Heroes';
+  public heroes: Hero[];
   public selectedHero: Hero;
-  
-  onSelect(hero: Hero) { 
-    this.selectedHero = hero; 
+  constructor(private _heroService: HeroService) { }
+  getHeroes() {
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
+
+
+
+
+
+
+
+
+
